e6385ac4845214df87e072af646b97c7bf3c0539
Anais Debureaux <anaisdeb00@gmail.com>
2022-11-25 17:40:28 +0100

    enable BLE communication with device

--- lib/Pages/bluetooth.dart
+++ lib/Pages/bluetooth.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_blue/flutter_blue.dart';
 
@@ -6,13 +9,18 @@ class BluetoothSettings extends StatefulWidget {
   BluetoothSettings({super.key});
   final FlutterBlue flutterBlue = FlutterBlue.instance;
   final List<BluetoothDevice> devicesList =  <BluetoothDevice>[];
+  final Map<Guid, List<int>> readValues = <Guid, List<int>>{};
+
   @override
   State<BluetoothSettings> createState() => _BluetoothSettingsState();
 }
 
 class _BluetoothSettingsState extends State<BluetoothSettings> {
+  final _writeController = TextEditingController();
+  BluetoothDevice? _connectedDevice;
+  List<BluetoothService> _services = [];
 
-  _addDeviceTolist(final BluetoothDevice device) {
+  _addDeviceToList(final BluetoothDevice device) {
     if (!widget.devicesList.contains(device)) {
       setState(() {
         widget.devicesList.add(device);
@@ -24,14 +32,14 @@ class _BluetoothSettingsState extends State<BluetoothSettings> {
   void initState() {
     super.initState();
     // Start scanning
-    widget.flutterBlue.startScan(timeout: Duration(seconds: 4));
+    widget.flutterBlue.startScan(timeout: const Duration(seconds: 4));
 
 // Listen to scan results
     widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
       // do something with scan results
       for (ScanResult result in results) {
-        print("found one");
-        _addDeviceTolist(result.device);
+        debugPrint("found one");
+        _addDeviceToList(result.device);
       }
     });
 
@@ -40,10 +48,10 @@ class _BluetoothSettingsState extends State<BluetoothSettings> {
   }
 
   ListView _buildListViewOfDevices() {
-    List<Container> containers = <Container>[];
+    List<Widget> containers = <Widget>[];
     for (BluetoothDevice device in widget.devicesList) {
       containers.add(
-        Container(
+        SizedBox(
           height: 50,
           child: Row(
             children: <Widget>[
@@ -60,7 +68,21 @@ class _BluetoothSettingsState extends State<BluetoothSettings> {
                   'Connect',
                   style: TextStyle(color: Colors.blue),
                 ),
-                onPressed: () {},
+                onPressed: () async {
+                  widget.flutterBlue.stopScan();
+                  try {
+                    await device.connect();
+                  } on PlatformException catch (e) {
+                    if (e.code != 'already_connected') {
+                      rethrow;
+                    }
+                  } finally {
+                    _services = await device.discoverServices();
+                  }
+                  setState(() {
+                    _connectedDevice = device;
+                  });
+                },
               ),
             ],
           ),
@@ -83,6 +105,164 @@ class _BluetoothSettingsState extends State<BluetoothSettings> {
       title: const Text('Bluetooth Settings'),
       backgroundColor: Colors.blueAccent,
     ),
-      body: _buildListViewOfDevices(),);
+      body: _buildView(),);
+  }
+
+  ListView _buildView() {
+    if (_connectedDevice != null) {
+      return _buildConnectDeviceView();
+    }
+    return _buildListViewOfDevices();
+  }
+
+  ListView _buildConnectDeviceView() {
+    List<Widget> containers = <Widget>[];
+
+    for (BluetoothService service in _services) {
+      List<Widget> characteristicsWidget = <Widget>[];
+
+      for (BluetoothCharacteristic characteristic in service.characteristics) {
+        characteristic.value.listen((value) {
+          // ignore: avoid_print
+          print(value);
+        });
+        characteristicsWidget.add(
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(characteristic.uuid.toString(), style: const TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    ..._buildReadWriteNotifyButton(characteristic),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text('Value: ${widget.readValues[characteristic.uuid]}'),
+                  ],
+                ),
+                const Divider(),
+              ],
+            ),
+          ),
+        );
+      }
+      containers.add(
+        ExpansionTile(
+            title: Text(service.uuid.toString()),
+            children: characteristicsWidget),
+      );
+    }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  List<ButtonTheme> _buildReadWriteNotifyButton(
+      BluetoothCharacteristic characteristic) {
+    List<ButtonTheme> buttons = <ButtonTheme>[];
+
+    if (characteristic.properties.read) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: ElevatedButton(
+              child: const Text('READ', style: TextStyle(color: Colors.white)),
+                onPressed: () async {
+                  var sub = characteristic.value.listen((value) {
+                    setState(() {
+                      widget.readValues[characteristic.uuid] = value;
+                    });
+                  });
+                  await characteristic.read();
+                  sub.cancel();
+                },
+            ),
+          ),
+        ),
+      );
+    }
+    if (characteristic.properties.write) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: ElevatedButton(
+              child: const Text('WRITE', style: TextStyle(color: Colors.white)),
+              onPressed: () async {
+                await showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text("Write"),
+                        content: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: TextField(
+                                controller: _writeController,
+                              ),
+                            ),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: const Text("Send"),
+                            onPressed: () {
+                              characteristic.write(utf8
+                                  .encode(_writeController.value.text));
+                              Navigator.pop(context);
+                            },
+                          ),
+                          TextButton(
+                            child: const Text("Cancel"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              },
+            ),
+          ),
+        ),
+      );
+    }
+    if (characteristic.properties.notify) {
+      buttons.add(
+        ButtonTheme(
+          minWidth: 10,
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: ElevatedButton(
+              child: const Text('NOTIFY', style: TextStyle(color: Colors.white)),
+                onPressed: () async {
+                  characteristic.value.listen((value) {
+                    widget.readValues[characteristic.uuid] = value;
+                  });
+                  await characteristic.setNotifyValue(true);
+                },
+            ),
+          ),
+        ),
+      );
+    }
+
+    return buttons;
   }
 }
